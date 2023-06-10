7b36b6c9ad5ee313d94ad4ec98c45eacc34254fa
jonathan.posada1 <jonathan.posada1@udea.edu.co>
2023-06-10 10:08:17 -0500

    update ising_class.py in dev branch

--- Ising_class.py
+++ Ising_class.py
@@ -3,6 +3,8 @@ import math
 import numpy as np
 from tqdm import tqdm
 import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+from numpy.random import rand
 
 class Ising2D:
     def __init__(self, L, T, nsteps, J, H):
@@ -19,13 +21,19 @@ class Ising2D:
                     for i in range(self.N)}
         self.S = [random.choice([1, -1]) for k in range(self.N)]
         self.R = [random.choice([1, -1]) for k in range(self.N)]
+        self.iso = self.ising_2d()
+
+    def iso2(self):
+        self.H = np.flip(self.H)
+        iso2 = self.ising_2d()
+        return iso2
 
     def x_y(self, k):
         y = k // self.L
         x = k - y * self.L
         return x, y
 
-    def ising2D(self):
+    def ising_2d(self):
         energy = 0
         N = self.L * self.L
         beta = 1.0 / self.T
@@ -45,7 +53,7 @@ class Ising2D:
         itera = []
 
         for i in tqdm(range(len(self.H))):
-            #Quiero guardar una copia de los snapshoots en una parte intermedia
+            #Quiero guardar una copia de los snapshots en una parte intermedia
             if i==int(len(self.H)*0.475):
                 Intermedio=S.copy()
             for step in range(self.nsteps):
@@ -76,47 +84,183 @@ class Ising2D:
             conf_int[x1][y1]=Intermedio[k]
 
         return R, conf1, S, conf, sum(E) / float(len(E) * N), E, Mag, E_1, itera, conf_int
-    
 
+
+    # función de ajuste
+    def tanh(self, x, a, b, c):
+        return a * np.tanh(b * x + c)
+
+    # Ajustemos la curva de la isotermas para T1 T2 y T3 y grafiquemos con subplots
+    def plot_ajuste(self):
+        popt, pcov = curve_fit(self.tanh, self.H, self.iso[6])
+
+        # graficar ajuste
+        plt.figure(figsize=(10, 8))
+        plt.plot(self.H, self.iso[6], label="T={}K".format(self.T), color="red")
+        plt.plot(self.H, self.tanh(self.H, *popt), '*', label="Ajuste", color="black")
+        plt.grid()
+        plt.legend()
+        plt.show()
+
+        return
+
+    # graficar histeresis
+    def plot_histeresis(self):
+        plt.figure(figsize=(10, 8))
+        plt.title("Histeresis para un ferromagneto en el modelo de ising en 2D", fontsize=20)
+        plt.plot(np.arange(self.H[0], self.H[-1], 0.5), self.iso[6], label="T={}K".format(self.T), color="green")
+        plt.plot(np.arange(self.H[-1], self.H[0], -0.5), self.iso2()[6], label="T={}K".format(self.T), color="black")
+        plt.xlabel("H", fontsize=20)
+        plt.ylabel("M", fontsize=20)
+        plt.xlim(-5, 5)
+        plt.legend()
+        plt.show()
+
+        return
+
+    # graficar magnetizacion
     def plot_magnetizacion(self):
         plt.figure(figsize=(10, 6))
-        plt.plot(self.H, self.ising2D()[6], 'o', color='red')
+        plt.plot(self.H, self.iso[6], 'o', color='red')
         plt.xlabel('Campo magnético')
         plt.ylabel('Magnetización')
         plt.title('Magnetización vs Campo magnético')
         plt.show()
 
+        return
+
+    # graficar energía
     def plot_energia(self):
         plt.figure(figsize=(10, 6))
-        plt.plot(self.H, self.ising2D()[5], 'o', color='blue')
+        plt.plot(self.H, self.iso[5], 'o', color='blue')
         plt.xlabel('Campo magnético')
         plt.ylabel('Energía')
         plt.title('Energía vs Campo magnético')
         plt.show()
 
+        return
+
+    # graficar
     def plot_energia_itera(self):
         plt.figure(figsize=(10, 6))
-        plt.plot(self.ising2D()[8], self.ising2D()[7], 'o', color='blue')
+        plt.plot(self.iso[8], self.iso[7], 'o', color='blue')
         plt.xlabel('Iteración')
         plt.ylabel('Energía')
         plt.title('Energía vs Iteración')
         plt.show()
 
+        return
+
+    # graficar configuraciones
     def plot_configuraciones(self):
         plt.subplot(1,3,1)
-        plt.imshow(self.ising2D()[1], extent=[0,self.L,0,self.L], interpolation='nearest')
+        plt.imshow(self.iso[1], extent=[0, self.L, 0, self.L], interpolation='nearest')
         plt.xlabel("x",fontsize=10)
         plt.ylabel("y",fontsize=10)
         plt.title("Configuración inicial",fontsize=14)
         plt.subplot(1,3,2)
-        plt.imshow(self.ising2D()[9], extent=[0,self.L,0,self.L], interpolation='nearest')
+        plt.imshow(self.iso[9], extent=[0, self.L, 0, self.L], interpolation='nearest')
         plt.xlabel("x",fontsize=10)
         plt.ylabel("y",fontsize=10)
         plt.title("Configuración intermedia",fontsize=14)
         plt.subplot(1,3,3)
-        plt.imshow(self.ising2D()[3], extent=[0,self.L,0,self.L], interpolation='nearest')
+        plt.imshow(self.iso[3], extent=[0, self.L, 0, self.L], interpolation='nearest')
         plt.xlabel("x",fontsize=10)
         plt.ylabel("y",fontsize=10)
         plt.title("Configuración final",fontsize=14)
         plt.show()
 
+        return
+
+class IsingTemp(Ising2D):
+    def __init__(self, L, T, nsteps, J, H, nt, eq_steps, mc_steps, t_array):
+        super().__init__(L, T, nsteps, J, H)
+        self.nt = nt
+        self.eq_steps = eq_steps
+        self.mc_steps = mc_steps
+        self.N = np.int(np.sqrt(L))
+        self.M = np.zeros(self.nt)
+        self.C = np.zeros(self.nt)
+        self.X = np.zeros(self.nt)
+        self.E = np.zeros(self.nt)
+        self.n1 = 1.0 / (mc_steps * self.N * self.N)
+        self.n2 = 1.0 / (mc_steps * self.N * self.N)
+        self.t_array = t_array
+
+
+    def initial_state(self):
+        """rGenera un estado aleatorio para una red de NxN """
+
+        state = 2 * np.random.randint(2, size=(int(self.N), int(self.N))) - 1
+        return state
+
+    def mc_move(self, config, beta):
+        r"""Movimiento de Monte Carlo usando el algoritmo de Metropolis"""
+
+        for i in range(self.N):
+            for j in range(self.N):
+                a = np.random.randint(0, self.N)
+                b = np.random.randint(0, self.N)
+                s = config[a, b]
+                nb = config[(a + 1) % self.N, b] + config[a, (b + 1) % self.N] + config[(a - 1) % self.N, b] + config[a, (b - 1) % self.N]
+                cost = 2 * s * nb
+
+                if cost < 0:
+                    s *= -1
+                elif np.random.rand() < np.exp(-cost * beta):
+                    s *= -1
+                config[a, b] = s
+        return config
+
+    def calc_energy(self, config):
+        r"""Energía de una configuración dada"""
+
+        energy = 0
+
+        for i in range(len(config)):
+            for j in range(len(config)):
+                S = config[i, j]
+                nb = config[(i+1)%self.N, j] + config[i,(j+1)%self.N] + config[(i-1)%self.N, j] + config[i,(j-1)%self.N]
+                energy += -nb * S
+
+        return energy/2.
+
+    def calc_mag(self,config):
+        r"""Magnetización de una configuración  dada"""
+
+        mag = np.sum(config)
+        return mag
+
+    def ciclo_t(self):
+        for tt in tqdm(range(self.nt)):
+            config = self.initial_state()
+
+            E1 = M1 = E2 = M2 = 0
+            iT = 1.0 / self.t_array[tt]
+            iT2 = iT * iT
+
+        for i in range(self.eq_steps):
+            self.mc_move(config, iT)
+
+        for i in range(self.mc_steps):
+            self.mc_move(config, iT)
+            ene = self.calc_energy(config)
+            mag = self.calc_mag(config)
+
+            E1 = E1 + ene
+            M1 = M1 + mag
+            M2 = M2 + mag * mag
+            E2 = E2 + ene * ene
+
+        # Guardar los valores de energía y magnetización
+        self.E[tt] = self.n1 * E1
+        self.M[tt] = self.n1 * M1
+        self.C[tt] = (self.n1 * E2 - self.n2 * E1 * E1) * iT2
+        self.X[tt] = (self.n1 * M2 - self.n2 * M1 * M1) * iT
+
+        # graficamos
+        plt.figure(figsize=(8, 6))
+        plt.scatter(self.t_array, np.abs(self.M), s = 50, marker='o')
+        plt.show()
+
+        return
--- execute_Ising.py
+++ execute_Ising.py
@@ -3,23 +3,28 @@
 import Ising_class as Ising
 import numpy as np
 
-L = 100 # Lado de la red
-T = 1.0 # Temperatura
-nsteps = 40000 # Número de pasos
-J = 0 # Intercambio
-H=np.arange(-25,25,0.5) #campo magnetico
-if __name__=='__main__':
+if __name__ == '__main__':
+    L = 100  # Lado de la red
+    T = 2.0  # Temperatura
+    nsteps = 40000  # Número de pasos
+    J = 1  # Intercambio
+    H = np.linspace(-25, 25, 100)  # Campo magnético
+
     print("Veamos las gráficas pedidas:")
+    #ising_model = Ising.Ising2D(L, T, nsteps, J, H)
 
-
-    ising_model = Ising.Ising2D(L, T, nsteps, J, H)
     #ising_model.plot_magnetizacion()
     #ising_model.plot_energia()
-    #ising_model.plot_energia_1()
-    ising_model.plot_configuraciones()
+    #ising_model.plot_energia_itera()
+    #ising_model.plot_ajuste()
+    #ising_model.plot_histeresis()
+    #ising_model.plot_configuraciones()
 
+    nt = 32
+    eq_steps = 2 ** 8
+    mc_steps = 2 ** 10
+    t_array = np.linspace(1., 4., nt)
 
-
-
-
-
+    print('Gráficas Dependientes de la Temperatura')
+    ejemplo = Ising.IsingTemp(L, T, nsteps, J, H, nt, eq_steps, mc_steps, t_array)
+    print(ejemplo.ciclo_t())
